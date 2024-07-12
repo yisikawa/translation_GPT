086a8fe54052f35294b5e815f1b73a945a77d1ef
yisikawa <yisikawa@ma.medias.ne.jp>
2024-07-12 15:41:46 +0900

    use audio

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ os.environ["OPENAI_API_KEY"] = st.secrets.OpenAIAPI.openai_api_key
 
 chat = ChatOpenAI(model="gpt-3.5-turbo")
 
+
 # プロンプトのテンプレート
 system_template = (
     "あなたは、{source_lang} を {target_lang}に翻訳する優秀な翻訳アシスタントです。翻訳結果以外は出力しないでください。"
@@ -33,6 +34,16 @@ if "response" not in st.session_state:
     st.session_state["response"]= ""
 
 # LLMとやりとりする関数
+
+def transcribe_audio_to_text(audio_bytes):
+    openai.api_key = API_KEY
+    with NamedTemporaryFile(delete=True, suffix=".wav") as temp_file:
+        temp_file.write(audio_bytes)
+        temp_file.flush()
+        with open(temp_file.name, "rb") as audio_file:
+            response = openai.Audio.transcribe("whisper-1", audio_file)
+    return response["text"]
+    
 def communicate():
     text = st.session_state["user_input"]
     response = chat(
@@ -42,6 +53,14 @@ def communicate():
     )
     st.session_state["response"] = response.content
 
+def audio_input():
+    audio_bytes = audio_recorder(pause_threshold=30)
+    
+    # Convert audio to text using OpenAI Whisper API
+    if audio_bytes:
+        transcript = transcribe_audio_to_text(audio_bytes)
+        st.write("Transcribed Text:", transcript)
+        
 # ユーザーインターフェイスの構築
 st.title("翻訳アプリ")
 st.write("LangChainを使った翻訳アプリです。")
@@ -49,6 +68,7 @@ st.write("LangChainを使った翻訳アプリです。")
 options = ["日本語", "英語", "スペイン語", "ドイツ語", "フランス語", "中国語"]
 source_lang = st.selectbox(label="翻訳元", options=options)
 target_lang = st.selectbox(label="翻訳先", options=options)
+st.button("音声入力",type="primary",on_click=audio_input)
 st.text_input("翻訳する文章を入力してください。", key="user_input")
 st.button("翻訳", type="primary", on_click=communicate)
 
